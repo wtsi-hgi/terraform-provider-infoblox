1710fd23bb1bd3c4466d5a1cd8f7ec27b6c62512
Joshua Randall <joshua.randall@sanger.ac.uk>
2017-11-10 22:18:58 +0000

    add 90s Timeout on HttpClient

--- infoblox/config.go
+++ infoblox/config.go
@@ -2,6 +2,7 @@ package infoblox
 
 import (
 	"log"
+	"time"
 
 	"github.com/fanatic/go-infoblox"
 )
@@ -18,6 +19,7 @@ type Config struct {
 // Client returns a new client for accessing Infoblox.
 func (c *Config) Client() (*infoblox.Client, error) {
 	client := infoblox.NewClient(c.Host, c.Username, c.Password, c.SSLVerify, c.UseCookies)
+	client.HttpClient.Timeout = 90 * time.Second
 
 	log.Printf("[INFO] Infoblox Client configured for user: %s", client.Username)
 
